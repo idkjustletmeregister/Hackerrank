2ae8fd45edb7f8ee21cbb8bf88f923b4d1c93f3c
idkjustletmeregister <114916288+idkjustletmeregister@users.noreply.github.com>
2023-06-06 15:53:13 +0200

    Update PlusMinus.py
    Lazier and more efficient solution ?

--- PlusMinus.py
+++ PlusMinus.py
@@ -14,19 +14,17 @@ import sys
 
 def plusMinus(arr,n):
     
-    pos, zer, neg = 0, 0, 0
+    pos, neg, zer = 0, 0, 0
     
-    for i in range(0,n):
-        if arr[i] == 0:
-            zer += 1
-        elif arr[i] < 0:
+    for num in arr:
+        if num < 0:
             neg += 1
-        elif arr[i] > 0:
+        elif num > 0:
             pos += 1
+        else:
+            zer += 1
     
-    print(float("{:.6f}".format( pos/n )))
-    print(float("{:.6f}".format( neg/n )))
-    print(float("{:.6f}".format( zer/n )))
+    print("%.6f\n%.6f\n%.6f" % (pos/n, neg/n, zer/n))
 
 
 if __name__ == '__main__':
